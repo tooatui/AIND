9aa2af358f4127c297872a0f028ca5118bb703fb
Linli Chen <linli.chen@outlook.com>
2018-03-11 17:44:10 -0700

    add initial script for 14_minimax

---
+++ Class Quiz/Week2/14_minimax.py
@@ -0,0 +1,26 @@
+# https://classroom.udacity.com/nanodegrees/nd889/parts/6be67fd1-9725-4d14-b36e-ae2b5b20804c/modules/f719d723-7ee0-472c-80c1-663f02de94f3/lessons/9b1a742a-fa2d-4940-922c-ed426b44f81b/concepts/977cc04a-50cc-41aa-8302-a6ecdc606c9d
+
+def terminal_test(gameState):
+    """ Return True if the game is over for the active player
+    and False otherwise.
+    """
+    # TODO: finish this function!
+    pass
+
+
+def min_value(gameState):
+    """ Return the value for a win (+1) if the game is over,
+    otherwise return the minimum value over all legal child
+    nodes.
+    """
+    # TODO: finish this function!
+    pass
+
+
+def max_value(gameState):
+    """ Return the value for a loss (-1) if the game is over,
+    otherwise return the maximum value over all legal child
+    nodes.
+    """
+    # TODO: finish this function!
+    pass
