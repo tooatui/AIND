5747d5808ac0304df50074c816e3458bcffb6d41
Linli Chen <linli.chen@outlook.com>
2018-03-12 21:44:49 -0700

    update with udacity solution
    lambda expression is used in max function

--- Class Quiz/Week2/19_minimax_search.py
+++ Class Quiz/Week2/19_minimax_search.py
@@ -1,6 +1,27 @@
 from minimax_helpers import *
 
+# Solution using an explicit loop based on max_value()
+def _minimax_decision(gameState):
+    """ Return the move along a branch of the game tree that
+    has the best possible value.  A move is a pair of coordinates
+    in (column, row) order corresponding to a legal move for
+    the searching player.
+    
+    You can ignore the special case of calling this function
+    from a terminal state.
+    """
+    best_score = float("-inf")
+    best_move = None
 
+    for move in gameState.get_legal_moves():
+    	value = min_value(gameState.forcase_move(move))
+    	if best_score < value:
+    		best_score = value
+    		best_move = move
+    return best_move
+
+# This solution does the same thing using the built-in `max` function
+# Note that "lambda" expressions are Python's version of anonymous functions
 def minimax_decision(gameState):
     """ Return the move along a branch of the game tree that
     has the best possible value.  A move is a pair of coordinates
@@ -10,7 +31,7 @@ def minimax_decision(gameState):
     You can ignore the special case of calling this function
     from a terminal state.
     """
-    best_move = max((move for move in gameState.get_legal_moves()), func=min_value)
-    return best_move
-
-
+    # The built in `max()` function can be used as argmax!
+    return max(gameState.get_legal_moves(), 
+    	key=lambda m: min_value(gameState.forcase_move(m)))
+    
